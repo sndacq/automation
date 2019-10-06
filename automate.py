ba14c2c034eb2c1a86d6ea4e0be3a108814915a4
sndacq <seandacquel@gmail.com>
2019-10-06 12:45:39 +0800

    Added complete login and reboot method

--- automate.py
+++ automate.py
@@ -1,29 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import time
 
-geckodriver = r'C:\\Python37\\geckodriver.exe'
-binary = r'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+geckodriver = '/path/to/geckodriver.exe'
+binary = '/path/to/firefox.exe'
+address = 'http://192.168.254.254/'
 
 username = ''
 password = ''
 
 options = Options()
-# options.headless = True
+options.headless = True
 options.binary = binary
 
 browser = webdriver.Firefox(options=options, executable_path=geckodriver)
-browser.get('http://192.168.254.254/')
 
-login = browser.find_element_by_id('logout_span')
-if login.text == 'Log In':
-    login.click()
-    browser.find_element_by_id('username').send_keys(username)
-    browser.find_element_by_id ('password').send_keys(password)
-    browser.find_element_by_id ('pop_login').click()
+try:
+    browser.get(address)
+    login = browser.find_element_by_id('logout_span')
 
+    if login.text == 'Log In':
+        print('Loggin in...')
+        login.click()
+        browser.find_element_by_id('username').send_keys(username)
+        browser.find_element_by_id('password').send_keys(password)
+        browser.find_element_by_id('pop_login').click()
+        print('Successfully logged in')
 
+        browser.get(f'{address}html/reboot.html')
+        browser.find_element_by_id('undefined').click()
+        browser.find_element_by_id('pop_confirm').click()
+        time.sleep(5)
+        print('Rebooting...')
 
+except Exception as e:
+    print(e)    
 
-
-
+print('Exit Script')
 browser.quit()
