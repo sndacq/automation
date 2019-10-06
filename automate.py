25daa375a452a614454a9006ef747b371e388a12
sndacq <seandacquel@gmail.com>
2019-10-06 18:54:36 +0800

    Added config file

--- automate.py
+++ automate.py
@@ -1,22 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import time
+from config import *
+from time import sleep
 
-geckodriver = '/path/to/geckodriver.exe'
-binary = '/path/to/firefox.exe'
-address = 'http://192.168.254.254/'
 
-username = ''
-password = ''
-
-options = Options()
-options.headless = True
-options.binary = binary
-
-browser = webdriver.Firefox(options=options, executable_path=geckodriver)
-
-try:
-    browser.get(address)
+def login(browser):
     login = browser.find_element_by_id('logout_span')
 
     if login.text == 'Log In':
@@ -27,14 +15,34 @@ try:
         browser.find_element_by_id('pop_login').click()
         print('Successfully logged in')
 
-        browser.get(f'{address}html/reboot.html')
-        browser.find_element_by_id('undefined').click()
-        browser.find_element_by_id('pop_confirm').click()
-        time.sleep(5)
-        print('Rebooting...')
+def reboot(browser):
+    browser.get(f'{address}html/reboot.html')
+    browser.find_element_by_id('undefined').click()
+    # TODO: add dynamic waiting for elements
+    sleep(3)
+    browser.find_element_by_id('pop_confirm').click()
+    sleep(3)
+    print('Rebooting...')    
 
-except Exception as e:
-    print(e)    
+def main():
+    options = Options()
+    options.headless = True
+    options.binary = binary
 
-print('Exit Script')
-browser.quit()
+    browser = webdriver.Firefox(options=options, executable_path=geckodriver)
+
+    try:
+        browser.get(address)
+        login(browser)
+        reboot(browser)
+
+    except Exception as e:
+        print(e)    
+
+    print('Exit Script')
+    browser.quit()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ config.py
@@ -0,0 +1,8 @@
+geckodriver = '/path/to/geckodriver.exe'
+binary = '/path/to/firefox.exe'
+address = 'http://gateway.address/'
+
+username = 'username'
+password = 'password'
+
+allowed_mac = [ '00:00:00:00:00:00', '00:00:00:00:00:00' ]
